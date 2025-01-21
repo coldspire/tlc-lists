6d76e6383fa90175b11b6f138623918f812aeba8
Owen <5332788+coldspire@users.noreply.github.com>
2025-01-21 08:29:24 -0500

    Convert JSON data files to JS files for flexibility (#6)

---
+++ _data/lists/favoriteAlbums2024.js
@@ -0,0 +1,33 @@
+export default {
+  title: "Favorite Albums of 2024",
+  description: [
+    "This is a line of the description. **whoa**",
+    "This second line contains a [link](www.google.com). Sweet"
+  ],
+  type: {
+    primary: "album",
+    secondary: "artist"
+  },
+  items: [
+    {
+      primary: "Wheels Within Wheels",
+      secondary: "Meer",
+      additionalText: `
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+        
+        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+      `
+    },
+    {
+      primary: "Fym",
+      secondary: "Azure",
+      additionalText: `
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+        
+        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
+        
+        Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Ut wisi enim ad minim
+      `
+    }
+  ]
+};
---
+++ _data/lists/favoriteSongs2024.js
@@ -0,0 +1,24 @@
+export default {
+  title: "Favorite Songs of 2024",
+  description: [
+    "This is a line of the description. **whoa**",
+    "This second line contains a [link](www.google.com). Sweet"
+  ],
+  isCountdown: true,
+  type: {
+    primary: "song",
+    secondary: "album and artist"
+  },
+  items: [
+    {
+      primary: "The Azdinist / Den of Dawns",
+      secondary: "From \"Fym\" by Azure",
+      additionalText: "This song rules. _Yes_"
+    },
+    {
+      primary: "No Weapon Formed Shall Prosper",
+      secondary: "From \"Eviscerate\" by Eidola",
+      additionalText: "What a song!"
+    }
+  ]
+}
