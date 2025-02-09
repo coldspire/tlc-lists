fcec57195170cde969290c5858364c73a188df27
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2025-02-09 14:52:36 -0500

    [in progress] Writing favorites

--- _data/lists/favoriteAlbums2024.js
+++ _data/lists/favoriteAlbums2024.js
@@ -1,68 +1,141 @@
 export default {
   title: "Favorite Albums of 2024",
-  description: [
-    "This is a line of the description. **whoa**",
-    "This second line contains a [link](www.google.com). Sweet"
+  "description": [
+    "While 2024 didn't bring as many bangers as the nearly-unparalleled super-stacked 2023*, 2024 turned out to be the years of surprises. Last year brought several best-of-career releases from the known suspects (_Sleep Token_, _Periphery_, _Tesseract_, _Gloryhammer_, others!), but this year brought equally-terrific albums by surprise guests, bands I've never heard of.",
+    "So if 2023 was the year where my own circle of music paid off, 2024 was the year where my circle broadended signficantly. The end result is two great years."
   ],
   type: {
     primary: "album",
     secondary: "artist"
   },
-  items: [
+  "isCountdown": true,
+  "items": [
     {
-      primary: "Wheels Within Wheels",
-      secondary: "Meer",
-      additionalText: `
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-        
-        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata`
+      primary: "A Boat on the Sea (2019)",
+      secondary: "Moron Police",
+      additionalText: `For some reason the weekly-curated "New Music" playlist on Apple Music brought me "The Dog Song" off this five-year-old album, and, boy, am I sure glad it did. That lone curation opened up a album of pure tight progressive-rock joy completely out of the blue. I listened to the album at least a dozen times digitally before importing the vinyl from a little house in the Netherlands countryside, and then I kept that platter spinning at least another dozen times. This is one of those albums that when I think about it, I want to listen to it immediately.`
     },
     {
       primary: "Fym",
       secondary: "Azure",
-      additionalText: `
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
-        
-        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
-        
-        Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Ut wisi enim ad minim
-      `
-    }
-  ],
-  supplements: [
-    {
-      name: "Honorable Mentions",
-      description: "Albums that just didn't make the cut.",
-      itemMarker: '☀️',
-      items: [
-        {
-          primary: "Pigs ",
-          secondary: "The Pig Band",
-          additionalText: `Rockin' pigs rocked too hard`
-        },
-        {
-          primary: "Hogs",
-          secondary: "Hogg Hog and the Hogs",
-          additionalText: `Too much snorting`
-        }
-      ]
+      additionalText: ``
     },
     {
-      name: "Honorable Misses",
-      description: "Albums that didn't make the cut for bad reasons.",
-      itemMarker: '⛔',
-      items: [
-        {
-          primary: "Burger King At Twilight",
-          secondary: "The Fast Foodies",
-          additionalText: `I don't get it`
-        },
-        {
-          primary: "Sins of the Fathers",
-          secondary: "The Mothers",
-          additionalText: `Just a lot of blame game`
-        }
-      ]
+      primary: "Swagger & Stroll Down the Rabbit Hole (2021)",
+      secondary: "Diablo Swing Orchestra",
+      additionalText: ``
+    },
+    {
+      primary: "Mannequin",
+      secondary: "Kyros",
+      additionalText: `Little did I know that my favorite Frost* album of the year would be by a totally different group. The Frost*-y influences here are clear, but so is the freshness, playfulness, and inventiveness that comes with every album by the original blokes. _Mannequin_ was my introduction to Kyros, and the collosal listening experience reminds me of when I first listened to _also_-English prog masters Haken and their major effort _The Mountain_: I've heard something spectacular, and I'll be keeping up on this band for a long, long time.`
+    },
+    {
+      primary: "The Likes of Us ",
+      secondary: "Big Big Train",
+      additionalText: `This album was my most-listened-to album in 2024. After its March 1st release, I ordered the vinyl set based on how many times I'd put on the "Miramare" single alone; after the arrival, for a couple weeks I started every work morning by spinning this album from beginning to end. I haven't always sat in on Big Big Train's hedgerows and pastures English-swaddled progressive rock (though I might count 2019's Grand Tour on the long list of all-time favorites). But this album has a folksy, er, richness that really hit the spot in 2024.`
+    },
+    {
+      primary: "Falter, Endure",
+      secondary: "Isbjörg",
+      additionalText: ``
+    },
+    {
+      primary: "Life in the Wires",
+      secondary: "Frost*",
+      additionalText: `One of the many great things about Frost*, a band I've followed since their landmark prog release _Milliontown_ in 2006, is that a new release means an album that both sounds like Frost* while it attempts a whole batch of things new to the band. So for _Life in the Wires_, the prominent new wrinkle is a concept album: a science-fiction concept about the search for a mysterious broadcast heard through a transistor radio. It sprawls a bit more than the usual Frost* LP, but it also compels more effectively than the usual song-based album. And so Frost* continues their streak of albums that know when to soar and when to stay firmly grounded, bouyed by the exceptional talent that knows the difference between playing big and just sounding big. And, man, I love a good concept album.`
+    },
+    {
+      primary: "Mile Marker Zero",
+      secondary: "Coming of Age",
+      additionalText: `We may never get a 00's-era Porcupine Tree album again, but the variety and confidence in this album by Mile Marker Zero makes me feel a lot better about the dismal chances of a _Deadwing 2_. This band has my periphery for several years, but _Coming of Age_ finally brought me in. Plenty of strong sounds here; the title track is a particular favorite.`
+    },
+    {
+      primary: "Clear Cold Beyond",
+      secondary: "Sonata Arctica",
+      additionalText: `An all-time favorite band soars again with _Clear Cold Beyond_ after hitting the skids with 2019's utterly tepid and forgettable _Talviyö_. Band leader Tony Kakko has mentioned that he takes fault and blame for the progressive and tonal meandering of the band for the past fifteen or so (which wasn't all bad, though Talviyö clearly showed that phase of the band ran out of gas), and this 2024 album was his attempt to get the band back on track. Though I'd never fault a band for trying new things and seeing where the songs take them – bands are made of people, and people change! – Sonata Arctica never really felt wholly comfortable in the many attempts at fitting into a progressive groove. So when Clear Cold Beyond trades prog for power, the foundation sounds stronger than it has in a long time, bringing forth a rollicking batch of power metal bangers. And since this album has a bit of a feel of these Finnish lads finding their feet again, I'm very much looking forward to what they do next.`
+    },
+    {
+      primary: "The Skies Above Eternity",
+      secondary: "Fellowship",
+      additionalText: `Fellowship released their first album _The Saberlight Chronicles_ in 2022, and that batch of power metal was a once-in-a-decade level release. If the band had decided to hang it up after that stellar freshman release, I would've been disappointed but understood completely. But here we are, two years later, and this young group of goofy English metallers brought us a new platter of happy power metal tunes. _The Skies Above Eternity_ isn't up to the level of _The Saberlight Chronicles_, but, boy, it is still a heck of a power metal album. Any other band would be happy and grateful to have this lone album in their back catalog; for Fellowship, they have it _and_ one even better. The sky's the limit with these boys.`
+    },
+    {
+      primary: "Neon Inferno",
+      secondary: "One Morning Left",
+      additionalText: ``
+    },
+    {
+      primary: "Rug Island",
+      secondary: "Bluey",
+      additionalText: ``
+    },
+    {
+      primary: "Wheels Within Wheels",
+      secondary: "Meer",
+      additionalText: ``
+    },
+    {
+      primary: "Where Gods Fear To Speak",
+      secondary: "Oceans of Slumber",
+      additionalText: ``
+    },
+    {
+      primary: "Solemn",
+      secondary: "In Vain",
+      additionalText: ``
+    },
+    {
+      primary: "Karma",
+      secondary: "Myrath",
+      additionalText: ``
+    },
+    {
+      primary: "Charcoal Grace",
+      secondary: "Caligula's Horse",
+      additionalText: ``
+    },
+    {
+      primary: "Charismatic Leaders",
+      secondary: "Wheel",
+      additionalText: ``
+    },
+    {
+      primary: "Les Chants de l'Aurore",
+      secondary: "Alcest",
+      additionalText: `Several years ago I saw _Alcest_ live as the opener for an _Anathema_ show, and this post-metal band cemented themselves in my musical history by playing so loud that they caused me permanent hearing damage. (I wore earplugs to concerts both before and after that one. So I had no excuse at the _Alcest_ show.) In 2024, these Frenchies released this fine album that an [Angry Metal Guy reviewer described as evoking late summer](https://www.angrymetalguy.com/alcest-les-chants-de-laurore-review/), and I wouldn't even try to come up with a better comparison. The LP has the embrace of seasonal warmth and the fading light as the colder months approach. _Chants_ feels like the memories of days in the sun are already beginning to fade. And the album art is gorgeous.`
+    },
+    {
+      primary: "Showdown",
+      secondary: "Mega Colossus",
+      additionalText: `An album of non-stop no-nonsense riffs and grooves, featuring the usual high-falutin' concepts for songwriting such as, uh, _Porco Rosso_, _Mad Max: Fury Road_, and _Face/Off_. Complete unabashed fun – you can imagine the giant grins of the musicians as they blast through track after track. Though I'm not typically hooked by adjective-less metal – no power, no death, not even any thrash, just capital-M Metal – this one has a speed and joy that hooks deep.`
+    },
+  ],
+  exclusions: [
+    {
+      primary: "Beaver",
+      secondary: "Dirty Loops",
+      additionalText: `A super-tight five songs from a band that hooks me more everytime I hear new stuff. I decided not to include it due to the EP nature of the release – putting it against a LP worth of material just didn't seem fair. `
+    },
+    {
+      primary: "Eviscerate",
+      secondary: "Eidola",
+      additionalText: `After the early-February release of _Eviscerate_ and its blistering single _No Weapon Formed May Prosper_, my most-listened-to song of 2024, I was convinced this rage-and-anger concept would coast into the top-albums list by year's end. Much to my surprise, when I revisited _Eviscerate_ at the end of the year, while I still found the first half the LP sharp and relentless as ever, the second half of the album repeatedly failed to keep my attention. An album that's only half killer is, well, not much of an album. Though a disappointment for this list, _Eviscerate_ still has a lot to enjoy, particularly in that scorching first half and the final full-length tune "Golgotha Compendium: Fifth Temple," a bookend that demonstrates Eidola can even play a bit in the progressive realm.`
+    },
+    {
+      primary: "The Last Will and Testament",
+      secondary: "Opeth",
+      "additionalText": "I've heard this is the band's best in a decade-plus, but so far I've almost completely missed out on it, giving it only one spin at the end of 2024. Maybe it'll end up on the best of 2025 list."
+    },
+    {
+      primary: "The Surface (Deluxe&nbsp;Edition)",
+      secondary: "Beartooth",
+      additionalText: `A bit pop, a bit metalcore, a bit Andrew W.K. A lot of fun but overstays its welcome by half an album. If "The Surface" was an EP of my favorites, it would easily be a top-albums list (which, really, can be said for a lot of albums). Still, this album brought me the bright and bold "ATTN," an eleventh-hour favorite song of the year.`
+    },
+    {
+      "primary": "Yesterwynde",
+      secondary: "Nightwish",
+      "additionalText": "I had given up on this band about two albums ago, due to a plodding album release cycle and songwriting that trended towards bloated orchestration and main man Tuomas' definciencies. Which is a bit sad, because Nightwish was one of the original bands to get me into power metal − and really, got me into metal as a whole, broadening my musical universe to this day − in the first place. So I was encouraged that Yesterwynde almost brought me back into the fold, the first time since 2011's Imaginaerum. Alas, it didn't win out against the other power-metal-ful releases of the year. But it does give me hope that Nightwish's next release will bring me back even closer to where it all started."
     }
   ]
 };
