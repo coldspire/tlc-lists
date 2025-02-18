bd8a9ef50bb01fc35af9917b5b807a1e2963da2a
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2025-02-18 14:43:21 -0500

    More writing

--- _data/lists/favoriteAlbums2024.js
+++ _data/lists/favoriteAlbums2024.js
@@ -2,7 +2,8 @@ export default {
   title: "Favorite Albums of 2024",
   "description": [
     "While 2024 didn't bring as many bangers as the nearly-unparalleled super-stacked 2023, 2024 turned out to be the years of surprises. A good half of the albums listed below are by bands I'd never _heard_ of, let alone heard any music by. Which means that I brought a ton of new bands, new sounds, and new music into the fold in 2024. A great year overall.",
-    "Somewhat curiously, eight of my picks hail from Nordic locales. Another six are from England. I guess I know where I like my music to come from."
+    "Somewhat curiously, eight of my picks hail from Nordic locales. Another six are from England. I guess I know where I like my music to come from.",
+    "Also, note that I don't restrict my list to albums _released_ in 2024, although most of the below albums were indeed new in 2024."
   ],
   type: {
     primary: "album",
@@ -43,7 +44,7 @@ export default {
     {
       primary: "Life in the Wires",
       secondary: "Frost*",
-      additionalText: `One of the many great things about Frost*, a band I've followed since their landmark prog release _Milliontown_ in 2006, is that a new release means an album that both sounds like Frost* while it attempts a whole batch of things new to the band. So for _Life in the Wires_, the prominent new wrinkle is a concept album: a science-fiction concept about the search for a mysterious broadcast heard through a transistor radio. It sprawls a bit more than the usual Frost* LP, but it also compels more effectively than the usual song-based album. And so Frost* continues their streak of albums that know when to soar and when to stay firmly grounded, bouyed by the exceptional talent that knows the difference between playing big and just sounding big. And, man, I love a good concept album.`
+      additionalText: `One of the many great things about Frost*, a band I've followed since their landmark prog release _Milliontown_ in 2006, is that a new release means an album that both sounds like Frost* while it attempts a whole batch of things new to the band. So for _Life in the Wires_, the prominent new wrinkle is a concept album: a science-fiction story about the search for a mysterious broadcast heard through a hardware transistor radio in a world where such things, radios and over-the-air transmissions, shouldn't exist. _Life_ sprawls a bit more than the usual Frost* LP, but it also drives ahead more effectively too. And so Frost* continues their streak of albums that know when to soar and when to stay firmly grounded, bouyed by the exceptional talent that knows the difference between playing big and just sounding big. And, man, I love a good concept album.`
     },
     {
       primary: "Mile Marker Zero",
@@ -51,14 +52,14 @@ export default {
       additionalText: `We may never get a 00's-era Porcupine Tree album again, but the variety and confidence in this album by Mile Marker Zero makes me feel a lot better about the dismal chances of a _Deadwing 2_. This band has my periphery for several years, but _Coming of Age_ finally brought me in. Plenty of strong sounds here; the title track is a particular favorite.`
     },
     {
-      primary: "Clear Cold Beyond",
-      secondary: "Sonata Arctica",
-      additionalText: `An all-time favorite band soars again with _Clear Cold Beyond_ after hitting the skids with 2019's utterly tepid and forgettable _Talviyö_. Band leader Tony Kakko has mentioned that he takes fault and blame for the progressive and tonal meandering of the band for the past fifteen or so (which wasn't all bad, though Talviyö clearly showed that phase of the band ran out of gas), and this 2024 album was his attempt to get the band back on track. Though I'd never fault a band for trying new things and seeing where the songs take them – bands are made of people, and people change! – Sonata Arctica never really felt wholly comfortable in the many attempts at fitting into a progressive groove. So when Clear Cold Beyond trades prog for power, the foundation sounds stronger than it has in a long time, bringing forth a rollicking batch of power metal bangers. And since this album has a bit of a feel of these Finnish lads finding their feet again, I'm very much looking forward to what they do next.`
-    },
-    {
       primary: "The Skies Above Eternity",
       secondary: "Fellowship",
       additionalText: `Fellowship released their first album _The Saberlight Chronicles_ in 2022, and that batch of power metal was a once-in-a-decade level release. If the band had decided to hang it up after that stellar freshman release, I would've been disappointed but understood completely. But here we are, two years later, and this young group of goofy English metallers brought us a new platter of happy power metal tunes. _The Skies Above Eternity_ isn't up to the level of _The Saberlight Chronicles_, but, boy, it is still a heck of a power metal album. Any other band would be happy and grateful to have this lone album in their back catalog; for Fellowship, they have it _and_ one even better. The sky's the limit with these boys.`
+    },
+    {
+      primary: "Clear Cold Beyond",
+      secondary: "Sonata Arctica",
+      additionalText: `An all-time favorite band soars again with _Clear Cold Beyond_ after hitting the skids with 2019's utterly tepid and forgettable _Talviyö_. Band leader Tony Kakko has mentioned that he takes fault and blame for the progressive and tonal meandering of the band for the past fifteen or so (which wasn't all bad, though Talviyö clearly showed that phase of the band ran out of gas), and this 2024 album was his attempt to get the band back on track. Though I'd never fault a band for trying new things and seeing where the songs take them – bands are made of people, and people change! – Sonata Arctica never really felt wholly comfortable in the many attempts at fitting into a progressive groove. So when Clear Cold Beyond trades prog for power, the foundation sounds stronger than it has in a long time, bringing forth a rollicking batch of power metal bangers. And since this album has a bit of a feel of these Finnish lads finding their feet again, I'm very much looking forward to what they do next.`
     },
     {
       primary: "Neon Inferno",
@@ -68,7 +69,7 @@ export default {
     {
       primary: "Rug Island",
       secondary: "Bluey",
-      additionalText: `I have all three vinyls for all three Bluey albums, including this one, in my collection. This new one ranks second-best in the collection, ranking lower than the excellent first album but higher than the sugar-rush effectiveness of _Dance Mode!_. The adapations and expansions to songs from the various episodes continues to be truly winning, as favorite tunes and memories get bigger and better, giving ample opporunity to showcase the top-tier songwriting and musicianship. Just like the show, and like the titular "Rug Island" episode, this album has playfulness, fun, and creativity, but also plenty of warmth. If this is the last album for the TV show – lead writer Joe Brumm is finishing his _Bluey_ involvement with a movie, leaving room for a movie album? – it's a great one to go out on.`
+      additionalText: `I have all three vinyls for all three Bluey albums, including this one, in my collection. This new one ranks second-best in the collection, ranking lower than the excellent first album but higher than the sugar-rush effectiveness of _Dance Mode!_. The adapations and expansions to songs from the various episodes continues to be truly winning, as favorite tunes and memories get bigger and better, giving ample opporunity to showcase the top-tier songwriting and musicianship. Just like the show, and like the titular "Rug Island" episode, this album has playfulness, fun, and creativity, but also plenty of warmth. If this is the last album for the TV show – lead writer Joe Brumm is finishing his _Bluey_ involvement with a movie, leaving room for a movie album, perhaps – it's a great one to go out on.`
     },
     {
       primary: "Wheels Within Wheels",
@@ -93,7 +94,7 @@ export default {
     {
       primary: "Charcoal Grace",
       secondary: "Caligula's Horse",
-      additionalText: `The liner notes for _Charcoal Grace_ mention how the band's previous album, _Rise Radiant_, was never fully known by the band by being released at the start of the COVID-19 epidemic, depriving it of tour-provided oxygen from the start. That album had a feeling of boldness, daring and frontier; _Charcoal Grace_, on the heels of its stillborn older sibling, feels like a family trying to find a way forward after a storm destroyed their house. It has strength but also somber realization and open wounds in the midst of recovery.`
+      additionalText: `The liner notes for _Charcoal Grace_ mention how the band's previous album, _Rise Radiant_, was never fully known by the band by being released at the start of the COVID-19 epidemic, depriving it of tour-provided oxygen from the start. That album had a feeling of boldness, daring and frontier; _Charcoal Grace_, on the heels of its stillborn older sibling, feels like a family trying to find a way forward after a storm destroyed their house. It has strength but also somber realization and open wounds in the midst of recovery. A weighty platter of songs, with "The Stormchaser" being one of my 2024 favorites.`
     },
     {
       primary: "Charismatic Leaders",
@@ -103,7 +104,7 @@ export default {
     {
       primary: "Les Chants de l'Aurore",
       secondary: "Alcest",
-      additionalText: `Several years ago I saw _Alcest_ live as the opener for an _Anathema_ show, and this post-metal band cemented themselves in my musical history by playing so loud that they caused me permanent hearing damage. (I wore earplugs to concerts both before and after that one. So I had no excuse at the _Alcest_ show.) In 2024, these Frenchies released this fine album that an [Angry Metal Guy reviewer described as evoking late summer](https://www.angrymetalguy.com/alcest-les-chants-de-laurore-review/), and I wouldn't even try to come up with a better comparison. The LP has the embrace of seasonal warmth and the fading light as the colder months approach. _Chants_ feels like the memories of days in the sun are already beginning to fade. And as the cherry on top, if this was a list of best album art, _Les Chants_ might've won the top spot.`
+      additionalText: `Several years ago I saw Alcest live as the opener for an Anathema show, and this post-metal band cemented themselves in my musical history by playing so loud that they caused me permanent hearing damage. (I wore earplugs to concerts both before and after that one. So I had no excuse at the Alcest show.) In 2024, these Frenchies released this fine album that an [Angry Metal Guy reviewer described as evoking late summer](https://www.angrymetalguy.com/alcest-les-chants-de-laurore-review/), and I wouldn't even try to come up with a better comparison. The LP has the embrace of seasonal warmth and the fading light as the colder months approach. It feels like the memories of days in the sun are already beginning to fade. And as the cherry on top, if this was a list of best album art, _Les Chants_ might've won the top spot.`
     },
     {
       primary: "Showdown",
@@ -131,6 +132,11 @@ export default {
       primary: "The Surface (Deluxe&nbsp;Edition)",
       secondary: "Beartooth",
       additionalText: `A bit pop, a bit metalcore, a bit Andrew W.K. A lot of fun but overstays its welcome by half an album. If "The Surface" was an EP of my favorites, it would easily be a top-albums list (which, really, can be said for a lot of albums). Still, this album brought me the bright and bold "ATTN," an eleventh-hour favorite song of the year.`
+    },
+    {
+      primary: "An Ocean Without A Shore",
+      secondary: "Weather Systems",
+      additionalText: "This one saddens me. This album, Danny Cavanagh's return to album-making after the collapse of Anathema and the subsequent precarious battle against the dark winter of severe mental illness, was easily in my top-three anticipated albums of the year. And yet it illicits almost no feeling out of me at all, exactly the opposite of what Anathema could bring forth song after song, album after album. I very much look forward to see what Dan can do next as he continues his recovery and builds his career back up. But as for this inaugral release, I feel totally disconnected from it."
     },
     {
       "primary": "Yesterwynde",
