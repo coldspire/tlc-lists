9228f9e5e5d6b83b8bba26bb35ff5448dea2f09b
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2025-03-31 21:58:01 -0400

    Writing for songs

--- _data/lists/favoriteSongs2024.js
+++ _data/lists/favoriteSongs2024.js
@@ -12,118 +12,122 @@ export default {
   items: [
     {
       primary: `The Azdinist / Den of Dawns`,
-      secondary: `on _Fym_ by Azure`,
+      secondary: `from _Fym_ by Azure`,
       additionalText: ``
     },
     {
-      primary: `The Dog Song`,
-      secondary: `on _A Boat on the Sea_ by Moron Police`,
+      primary: `The Dog Song (2019)`,
+      secondary: `from _A Boat on the Sea_ by Moron Police`,
       additionalText: ``
     },
     {
-      primary: `Follow the Light`,
-      secondary: `on _Turbo_ by Cory Wong, Dirty Loops`,
+      primary: `Follow the Light (2021)`,
+      secondary: `from _Turbo_ by Cory Wong, Dirty Loops`,
       additionalText: ``
     },
     {
       primary: `Miramare`,
-      secondary: `on _The Likes of Us_ by Big Big Train`,
+      secondary: `from _The Likes of Us_ by Big Big Train`,
       additionalText: ``
     },
     {
       primary: `No Weapon Formed Shall Prosper`,
-      secondary: `on _Eviscerate_ by Eidola`,
+      secondary: `from _Eviscerate_ by Eidola`,
       additionalText: ``
     },
     {
       primary: `Top 10 staTues tHat CriEd bloOd`,
-      secondary: `on _POST HUMAN: NeX GEn_ by Bring Me The Horizon`,
+      secondary: `from _POST HUMAN: NeX GEn_ by Bring Me The Horizon`,
       additionalText: ``
     },
     {
       primary: `The Stormchaser`,
-      secondary: `on _Charcoal Grace_ by Caligua's Horse`,
+      secondary: `from _Charcoal Grace_ by Caligua's Horse`,
       additionalText: ``
     },
     {
       primary: `Golden Circle`,
-      secondary: `on _Wheels Within Wheels_ by Meer`,
+      secondary: `from _Wheels Within Wheels_ by Meer`,
       additionalText: ``
     },
     {
       primary: `The End in Mind`,
-      secondary: `on _Mannequin_ by Kyros`,
+      secondary: `from _Mannequin_ by Kyros`,
       additionalText: ``
     },
     {
       primary: `Coming of Age`,
-      secondary: `on _Coming of Age_ by Mile Marker Zero`,
-      additionalText: ``
+      secondary: `from _Coming of Age_ by Mile Marker Zero`,
+      additionalText: `Draws me in with a combination of Gazpacho's mystical poetics and Porcupine Tree's rock stability. XXXXXXXXXXXXXXXXXXX
+       
+       Also, in the realm of how music hits us in strange ways: This song has one of the most simple and effective piano chord progressions I've heard in a long, long time. It's just a few chords, located around the middle of the song, but it's, like, _perfect_. Every time I hear the first two chords, I'm on the edge of my seat waiting for that third chord, even though I know exactly how it goes. And when that final chord lands, it's _just right._`
     },
     {
       primary: `Life in the Wires, Pt. 1`,
-      secondary: `on _Life in the Wires_ by Frost*`,
+      secondary: `from _Life in the Wires_ by Frost*`,
       additionalText: ``
     },
     {
       primary: `Emerald Dragon`,
-      secondary: `on _Neon Inferno_ by One Morning Left`,
+      secondary: `from _Neon Inferno_ by One Morning Left`,
       additionalText: ``
     },
     {
       primary: `Solitare`,
-      secondary: `on _Falter, Endure_ by Isbjörg`,
-      additionalText: ``
+      secondary: `from _Falter, Endure_ by Isbjörg`,
+      additionalText: `Come for the danceable math prog/rock/pop, and be rewarded with a saxophone solo. I was a big fan of Isbjörg's [2024 LP](/favoritealbums2024/#6), and this song is the apex of the melodic progressiveness on the album.`
     },
     {
       primary: `Submission`,
-      secondary: `on _Charismatic Leaders_ by Wheel`,
+      secondary: `from _Charismatic Leaders_ by Wheel`,
       additionalText: ``
     },
     {
       primary: `Dawnbreaker`,
-      secondary: `on _The Skies Above Eternity_ by Fellowship`,
+      secondary: `from _The Skies Above Eternity_ by Fellowship`,
       additionalText: ``
     },
     {
       primary: `Flamme Jumelle`,
-      secondary: `on _Les Chants de l'Aurore_ by Alcest`,
+      secondary: `from _Les Chants de l'Aurore_ by Alcest`,
       additionalText: ``
     },
     {
       primary: `Raijin & Fujin`,
-      secondary: `on _Ryujin_ by Ryujin`,
+      secondary: `from _Ryujin_ by Ryujin`,
       additionalText: ``
     },
     {
       primary: `Dance With The Devil`,
-      secondary: `on _Netherworld_ by Louise Patricia Crane`,
+      secondary: `from _Netherworld_ by Louise Patricia Crane`,
       additionalText: ``
     },
     {
       primary: `Octupus`,
-      secondary: `on _Rug Island_ by Bluey`,
+      secondary: `from _Rug Island_ by Bluey`,
       additionalText: ``
     },
     {
       primary: `First In Line`,
-      secondary: `on _Clear Cold Beyond_ by Sonata Arctica`,
+      secondary: `from _Clear Cold Beyond_ by Sonata Arctica`,
       additionalText: ``
     },
     {
       primary: `Battle Of Arcane Might (Re-Recorded 2024)`,
-      secondary: `on _Battle Of Arcane Might - Single_ by Twilight Force`,
+      secondary: `from _Battle Of Arcane Might - Single_ by Twilight Force`,
       additionalText: ``
     },
     {
       primary: `An Ocean Of Strange Islands`,
-      secondary: `on _Yesterwynde_ by Nightwish`,
-      additionalText: ``
+      secondary: `from _Yesterwynde_ by Nightwish`,
+      additionalText: `This is the first Nightwish song I've enjoyed in several years. Nightwish and I just don't gel as well in recent years, due to the band songwriting moving away from the gothic power/prog roots and more towards towards sweeping orchestration and symphony. But "An Ocean Of Strange Islands" brings me back some of that big dark Nightwish power, where they do a type of grounded symphonic power metal that soars and sweeps but never strays too far from a devilish grimness. 
+      
+      Overall Nightwish moved on, and I didn't keep up. But feeling some of the old spark that only Nightwish could ignite sure is nice.`
     },
     {
       primary: `Watch the World Burn`,
-      secondary: `on _Popular Monster_ by Falling In Reverse`,
-      additionalText: ``
+      secondary: `from _Popular Monster_ by Falling In Reverse`,
+      additionalText: `This manic song was my first taste of _Falling In Reverse_, bandleader Ronnie Radke's bad-boi revue. It was and still is good for a sugar rush, in part because of its dedication to the two halves of the bit: It starts as full-throated hip hop and ends as an over-driven metal cranker. In the end, the LP where this single resided was ultimately indigestible, with its self-aggrandizement and tough-guy posturing cranked up to an intolerable degree. But, man, this song is pretty dang loud and pretty dang fun.`
     }
   ]
 };
