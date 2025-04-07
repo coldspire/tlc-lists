f44112af51c82171914f83fd76f18c08a4c53def
Owen <5332788+coldspire@users.noreply.github.com>
2025-04-06 21:58:48 -0400

    In-progress stuff, additional-text fixes, and fav-albums 2025 (#17)
    * Add list note for an in-progress list
    
    * Update badge display in index
    
    * Reduce between-lists margin that was too high
    
    * Fix "in-progress" badge and templating missed the first time around
    
    * Fix bug where no item additional text won't build
    
    * Add favorite albums 2025 list

---
+++ _data/lists/favoriteAlbums2025.js
@@ -0,0 +1,103 @@
+export default {
+  title: "Favorite Albums of 2025",
+  "description": [],
+  type: {
+    primary: "album",
+    secondary: "artist"
+  },
+  status: "in-progress",
+  isCountdown: false,
+  items: [],
+  supplements: [
+    {
+      name: "Up for Consideration",
+      description: [
+        `Keeping track of potential favorites as 2025 progresses.`,
+        `Last updated on April 6, 2025.`
+      ],
+      itemMarker: '🤔',
+      items: [
+        {
+          primary: "A World Without Us",
+          secondary: "Myriad Drone"
+        },
+        {
+          primary: "Alkaios • Part I • The Eagle [2023]",
+          secondary: "NevBorn"
+        },
+        {
+          primary: "Alkaios • Part II • The Peacock",
+          secondary: "NevBorn"
+        },
+        {
+          primary: "Birna",
+          secondary: "Wardruna"
+        },
+        {
+          primary: "Concord",
+          secondary: "Judicator"
+        },
+        {
+          primary: "Emberblow",
+          secondary: "Orpheus Omega"
+        },
+        {
+          primary: "Eyes Of The Living Night",
+          secondary: "Jonathan Hultén"
+        },
+        {
+          primary: "Give Us The Moon",
+          secondary: "The Night Flight Orchestra"
+        },
+        {
+          primary: "The Unguided",
+          secondary: "Hellven"
+        },
+        {
+          primary: "Here Be Dragons",
+          secondary: "Avantasia"
+        },
+        {
+          primary: "Mend",
+          secondary: "Eidola"
+        },
+        {
+          primary: "Power Train",
+          secondary: "Majestica"
+        },
+        {
+          primary: "Shards",
+          secondary: "Dim Gray"
+        },
+        {
+          primary: "The Dormant Stranger",
+          secondary: "Disarmonia Mundi"
+        },
+        {
+          primary: "The Father of Make Believe",
+          secondary: "Coheed and Cambria"
+        },
+        {
+          primary: "The Grim Village",
+          secondary: "Object Unto Earth"
+        },
+        {
+          primary: "The Ossuary Lens",
+          secondary: "Allegaeon"
+        },
+        {
+          primary: "The Pressure Point",
+          secondary: "Ollie Wride"
+        },
+        {
+          primary: "Trust [EP]",
+          secondary: "Voidchaser"
+        },
+        {
+          primary: "Tsunami Sea",
+          secondary: "Spiritbox"
+        }
+      ]
+    }
+  ]
+};
--- src/index.md
+++ src/index.md
@@ -6,16 +6,24 @@ docTitle: Lists
 # Lists
 
 <ul class="list list--no-style">
-{% assign listIds = lists | keys %}
+{%- assign listIds = lists | keys -%}
 {%- for listId in listIds -%}
-{% assign list = lists[listId] %}
+{%- assign list = lists[listId] -%}
     <li class="list__item">
-        {% assign listTitle = list.title %}
-        {% if list.status == "draft" %}
-        {{ listTitle }} <span class="badge badge--draft">coming soon</span>
-        {% else %}
-        <a href="{{ listId | slugify }}/">{{ listTitle }}</a> <span class="badge badge--released">released</span>
-        {% endif %}
+        {%- assign listTitle = list.title -%}
+        {%- if list.status == "draft" -%}
+        {{ listTitle }} 
+        {%- else -%}
+        <a href="{{ listId | slugify }}/">{{ listTitle }}</a> 
+        {%- endif -%}
+        {%- case list.status -%}
+            {%- when "draft" -%}
+                <span class="badge badge--draft">coming soon</span>
+            {%- when "in-progress" -%}
+                <span class="badge badge--in-progress">in progress</span>
+            {%- when "released" -%}
+                <span class="badge badge--released">released</span>
+        {%- endcase -%}
     </li>
 {%- endfor -%}
 </ul>
