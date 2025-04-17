6476db2bf8704e92439069d6474ace18cd103e1c
Owen <5332788+coldspire@users.noreply.github.com>
2025-04-16 20:49:16 -0400

    Write favorite songs for 2024; fix Markdown in secondary text not rendering (#18)
    * Fix Markdown not rendering in secondary text
    
    * Writing for songs
    
    * Finalize list without write-ups

--- _data/lists/favoriteSongs2024.js
+++ _data/lists/favoriteSongs2024.js
@@ -1,9 +1,9 @@
 export default {
   title: "Favorite Songs of 2024",
   description: [
-    "WRITE ME"
+    `Not surprisingly, metal represents massively on this favorites list. But as with any year, when I venture out my favorite genre, such as when I did with the jazz-funk of "Follow the Light"([#3](#3)), the progressive pop of "Golden Circle" ([#9](#9)), and the dark folksy, er, progressive pop of "Dancing with the Devil" ([#19](#19)), the listening rewards are massive.`
   ],
-  status: "draft",
+  status: "released",
   isCountdown: true,
   type: {
     primary: "song",
@@ -12,117 +12,122 @@ export default {
   items: [
     {
       primary: `The Azdinist / Den of Dawns`,
-      secondary: `on _Fym_ by Azure`,
+      secondary: `from _Fym_ by Azure`,
       additionalText: ``
     },
     {
-      primary: `The Dog Song`,
-      secondary: `on _A Boat on the Sea_ by Moron Police`,
+      primary: `The Dog Song (2019)`,
+      secondary: `from _A Boat on the Sea_ by Moron Police`,
       additionalText: ``
     },
     {
-      primary: `Follow the Light`,
-      secondary: `on _Turbo_ by Cory Wong, Dirty Loops`,
+      primary: `Follow the Light (2021)`,
+      secondary: `from _Turbo_ by Cory Wong, Dirty Loops`,
       additionalText: ``
     },
     {
       primary: `Miramare`,
-      secondary: `on _The Likes of Us_ by Big Big Train`,
+      secondary: `from _The Likes of Us_ by Big Big Train`,
+      additionalText: ``
+    },
+    {
+      primary: `ATTN.`,
+      secondary: `from _The Surface (Deluxe Edition)_ by Beartooth`,
       additionalText: ``
     },
     {
       primary: `No Weapon Formed Shall Prosper`,
-      secondary: `on _Eviscerate_ by Eidola`,
+      secondary: `from _Eviscerate_ by Eidola`,
       additionalText: ``
     },
     {
       primary: `Top 10 staTues tHat CriEd bloOd`,
-      secondary: `on _POST HUMAN: NeX GEn_ by Bring Me The Horizon`,
+      secondary: `from _POST HUMAN: NeX GEn_ by Bring Me The Horizon`,
       additionalText: ``
     },
     {
       primary: `The Stormchaser`,
-      secondary: `on _Charcoal Grace_ by Caligua's Horse`,
+      secondary: `from _Charcoal Grace_ by Caligua's Horse`,
       additionalText: ``
     },
     {
       primary: `Golden Circle`,
-      secondary: `on _Wheels Within Wheels_ by Meer`,
+      secondary: `from _Wheels Within Wheels_ by Meer`,
       additionalText: ``
     },
     {
       primary: `The End in Mind`,
-      secondary: `on _Mannequin_ by Kyros`,
+      secondary: `from _Mannequin_ by Kyros`,
       additionalText: ``
     },
     {
       primary: `Coming of Age`,
-      secondary: `on _Coming of Age_ by Mile Marker Zero`,
+      secondary: `from _Coming of Age_ by Mile Marker Zero`,
       additionalText: ``
     },
     {
       primary: `Life in the Wires, Pt. 1`,
-      secondary: `on _Life in the Wires_ by Frost*`,
+      secondary: `from _Life in the Wires_ by Frost*`,
       additionalText: ``
     },
     {
       primary: `Emerald Dragon`,
-      secondary: `on _Neon Inferno_ by One Morning Left`,
+      secondary: `from _Neon Inferno_ by One Morning Left`,
       additionalText: ``
     },
     {
       primary: `Solitare`,
-      secondary: `on _Falter, Endure_ by Isbjörg`,
+      secondary: `from _Falter, Endure_ by Isbjörg`,
       additionalText: ``
     },
     {
       primary: `Submission`,
-      secondary: `on _Charismatic Leaders_ by Wheel`,
+      secondary: `from _Charismatic Leaders_ by Wheel`,
       additionalText: ``
     },
     {
       primary: `Dawnbreaker`,
-      secondary: `on _The Skies Above Eternity_ by Fellowship`,
+      secondary: `from _The Skies Above Eternity_ by Fellowship`,
       additionalText: ``
     },
     {
       primary: `Flamme Jumelle`,
-      secondary: `on _Les Chants de l'Aurore_ by Alcest`,
+      secondary: `from _Les Chants de l'Aurore_ by Alcest`,
       additionalText: ``
     },
     {
       primary: `Raijin & Fujin`,
-      secondary: `on _Ryujin_ by Ryujin`,
+      secondary: `from _Ryujin_ by Ryujin`,
       additionalText: ``
     },
     {
       primary: `Dance With The Devil`,
-      secondary: `on _Netherworld_ by Louise Patricia Crane`,
+      secondary: `from _Netherworld_ by Louise Patricia Crane`,
       additionalText: ``
     },
     {
-      primary: `Octupus`,
-      secondary: `on _Rug Island_ by Bluey`,
+      primary: `Octopus`,
+      secondary: `from _Rug Island_ by Bluey`,
       additionalText: ``
     },
     {
       primary: `First In Line`,
-      secondary: `on _Clear Cold Beyond_ by Sonata Arctica`,
+      secondary: `from _Clear Cold Beyond_ by Sonata Arctica`,
       additionalText: ``
     },
     {
       primary: `Battle Of Arcane Might (Re-Recorded 2024)`,
-      secondary: `on _Battle Of Arcane Might - Single_ by Twilight Force`,
+      secondary: `from _Battle Of Arcane Might - Single_ by Twilight Force`,
       additionalText: ``
     },
     {
       primary: `An Ocean Of Strange Islands`,
-      secondary: `on _Yesterwynde_ by Nightwish`,
+      secondary: `from _Yesterwynde_ by Nightwish`,
       additionalText: ``
     },
     {
       primary: `Watch the World Burn`,
-      secondary: `on _Popular Monster_ by Falling In Reverse`,
+      secondary: `from _Popular Monster_ by Falling In Reverse`,
       additionalText: ``
     }
   ]
