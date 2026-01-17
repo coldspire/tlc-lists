8514e12d49935d9d2af6116c20cbfe77d350bd58
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2026-01-17 17:44:11 -0500

    Writing, part one

--- _data/lists/favoriteAlbums2025.js
+++ _data/lists/favoriteAlbums2025.js
@@ -1,220 +1,71 @@
 export default {
-  title: "Favorite Albums of 2025",
-  "description": [],
+  title: "Top Albums of 2025",
+  "description": [
+    "I was anticipating an average year of music in 2025, but instead the year blessed us with one of the most stacked run of releases in recent memory.",
+    "2025 brought us a few big anticipated deliveries, old favorites, new favorites, nice surprises, mild surprises, eyebrow-raisers, head-scratchers, and a few disappointments. And at the tail end, an album was released that became one of my all-time favorites."
+  ],
   type: {
     primary: "album",
     secondary: "artist"
   },
-  status: "in-progress",
-  isCountdown: false,
-  items: [],
+  status: "released",
+  isCountdown: true,
+  items: [
+    { primary: "Pachinko", secondary: "Moron Police" },
+    { primary: "Hidden Realms", secondary: "Valiant Hearts" },
+    { primary: "The Sleeping City", secondary: "An Abstract Illusion" },
+    { primary: "Yonder (2022)", secondary: "Valiant Hearts" },
+    { primary: "Birna", secondary: "Wardruna" },
+    { primary: "Feary Tales for Strange Lullabies: The Dome (2021)", secondary: "6:33" },
+    { primary: "Eyes of the Living Night", secondary: "Jonathan Hulten" },
+    { primary: "Long Distance Station to Station (2021)", secondary: "Corey Mastrangelo" },
+    { primary: "Even In Arcadia", secondary: "Sleep Token" },
+    { primary: "Tunnel Vision", secondary: "Beach Bunny" },
+    { primary: "The Grim Village", secondary: "Object Unto Earth" },
+    { primary: "A La Sala", secondary: "Khurangbin" },
+    { primary: "May the Bridges We Burn Light the Way", secondary: "Omnium Gatherum" },
+    { primary: "Alkaios - Part 2 - The Peacock", secondary: "Nevborn" },
+    { primary: "Alunea", secondary: "Kardashev" },
+    { primary: "Receptor", secondary: "Dissona" },
+    { primary: "Fimbulvinter", secondary: "Bjørn Riis" },
+    { primary: "The Father of Make Believe", secondary: "Coheed and Cambria" },
+    { primary: "Shards", secondary: "Dim Gray" },
+    { primary: "the sugar machine", secondary: "slenderbodies" },
+    { primary: "Sable, Fable", secondary: "Bon Iver" },
+    { primary: "Vidrada", secondary: "Obiymy Doschu" },
+    { primary: "Corporation P.O.P", secondary: "Hot Milk" },
+    { primary: "Resurface", secondary: "Kalax" },
+    { primary: "Sunsquabi", secondary: "Sunsquabi" }
+  ],
   supplements: [
     {
-      name: "Up for Consideration",
-      description: [
-        `Keeping track of potential favorites as 2025 progresses.`,
-        `Last updated on July 23, 2025.`
-      ],
-      itemMarker: '🤔',
+      name: "Honorable mentions",
+      description: [],
+      itemMarker: "👍",
       items: [
-        {
-          primary: "A World Without Us",
-          secondary: "Myriad Drone"
-        },
-        {
-          primary: "Alkaios • Part I • The Eagle [2023]",
-          secondary: "NevBorn"
-        },
-        {
-          primary: "Alkaios • Part II • The Peacock",
-          secondary: "NevBorn"
-        },
-        {
-          primary: "Birna",
-          secondary: "Wardruna"
-        },
-        {
-          primary: "Concord",
-          secondary: "Judicator"
-        },
         {
           primary: "Emberglow",
           secondary: "Orpheus Omega"
-        },
-        {
-          primary: "Eyes Of The Living Night",
-          secondary: "Jonathan Hultén"
-        },
-        {
-          primary: "Give Us The Moon",
-          secondary: "The Night Flight Orchestra"
-        },
-        {
-          primary: "The Unguided",
-          secondary: "Hellven"
-        },
-        {
-          primary: "Mend",
-          secondary: "Eidola"
-        },
-        {
-          primary: "Shards",
-          secondary: "Dim Gray"
-        },
-        {
-          primary: "The Dormant Stranger",
-          secondary: "Disarmonia Mundi"
-        },
-        {
-          primary: "The Father of Make Believe",
-          secondary: "Coheed and Cambria"
-        },
-        {
-          primary: "The Grim Village",
-          secondary: "Object Unto Earth"
-        },
-        {
-          primary: "The Ossuary Lens",
-          secondary: "Allegaeon"
-        },
-        {
-          primary: "The Pressure Point",
-          secondary: "Ollie Wride"
-        },
-        {
-          primary: "Trust [EP]",
-          secondary: "Voidchaser"
-        },
-        {
-          primary: "Tsunami Sea",
-          secondary: "Spiritbox"
-        },
-        {
-          primary: "Civilisation (2018)",
-          secondary: "Southern Empire"
-        },
-        {
-          primary: "Tales Of A Dying Sun",
-          secondary: "Ash Twin Project"
-        },
-        {
-          primary: "Room On The Porch",
-          secondary: "Taj Mahal & Keb' Mo'"
-        },
-        {
-          primary: "Even In Arcadia",
-          secondary: "Sleep Token"
-        },
-        {
-          primary: "Fimbulvinter",
-          secondary: "Bjørn Riis"
-        },
-        {
-          primary: "Evangeline vs. The Machine",
-          secondary: "Eric Church"
-        },
-        {
-          primary: "The Third Sleep",
-          secondary: "Oak"
-        },
-        {
-          primary: "Will You Haunt Me, With That Same Patience",
-          secondary: "Bury Tomorrow"
-        },
-        {
-          primary: "Glasgow Love Story",
-          secondary: "Tide Lines"
-        },
-        {
-          primary: "Alunea",
-          secondary: "Kardashev"
-        },
-        {
-          primary: "Variola Vera (2021)",
-          secondary: "Himmellgeme"
-        },
-        {
-          primary: "Feary Tales For Strange Lullabies: The Dome",
-          secondary: "6:33"
-        },
-        {
-          primary: "Cinder",
-          secondary: "Lux Terminus"
-        },
-        {
-          primary: "The Harmony Machine",
-          secondary: "Teramaze"
-        },
-        {
-          primary: "SABLE, fABLE",
-          secondary: "Bon Iver"
-        },
-        {
-          primary: "Aspiral",
-          secondary: "Epica"
-        },
-        {
-          primary: "Let There Be Dark",
-          secondary: "Tower"
-        },
-        {
-          primary: "God of Angels Trust",
-          secondary: "Volbeat"
-        },
-        {
-          primary: "Відрада",
-          secondary: "Obiymy Doschu"
         },
         {
           primary: "Until Fear No Longer Defines Us (2009)",
           secondary: "Ghost Brigade"
         },
         {
-          primary: "A Random Moment of Stillness",
-          secondary: "Sublunar"
+          primary: "The Harmony Machine",
+          secondary: "Teramaze"
         },
         {
-          primary: "The Thunderfist Chronicles",
-          secondary: "Alestorm"
-        },
-        {
-          primary: "A LA SALA",
-          secondary: "Khruangbin"
-        },
-        {
-          primary: "Corporation P.O.P.",
-          secondary: "Hot Milk"
-        },
-        {
-          primary: "Regenesis",
-          secondary: "Celestial Wizard"
-        },
-        {
-          primary: "Edge of the Abyss",
-          secondary: "Calva Louise"
+          primary: "Room On The Porch",
+          secondary: "Taj Mahal & Keb' Mo'"
         }
       ]
     },
     {
-      name: "Notable",
-      description: [
-        `These 2025 releases are _not_ under consideration for a top album, but they are still worth writing about.`
-      ],
-      itemMarker: '🤷‍♂️',
-      items: [
-        {
-          primary: "PowerNerd",
-          secondary: "Devin Townsend"
-        },
-        {
-          primary: "Here Be Dragons",
-          secondary: "Avantasia"
-        },
-        {
-          primary: "Power Train",
-          secondary: "Majestica"
-        }
-      ]
+      name: "Disappointments and head-scratchers",
+      description: [],
+      itemMarker: "👎",
+      items: []
     }
   ]
 };
