f34dbbd3e338fc80d184b92f1ebe96374b168ff8
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2026-01-28 15:55:49 -0500

    Writing, part three

--- _data/lists/favoriteAlbums2025.js
+++ _data/lists/favoriteAlbums2025.js
@@ -41,7 +41,7 @@ export default {
   ],
   supplements: [
     {
-      name: "Honorable mentions",
+      name: "Honorable Mentions",
       description: [],
       itemMarker: "👍",
       items: [
@@ -176,20 +176,48 @@ export default {
       ]
     },
     {
-      name: "Stuff That Just Didn't Grab Me",
+      name: "Dishonorable Mentions",
       description: [],
       itemMarker: "🤷‍♂️",
       items: [
-        { primary: "Borderland", secondary: "Amorphis" },
-        { primary: "Here There Be Dragons", secondary: "Avantasia" },
-        { primary: "Futique", secondary: "Biffy Clyro" },
-        { primary: "Will You Haunt Me, With That Same Patience", secondary: "Bury Tomorrow" },
-        { primary: "Parasomnia", secondary: "Dream Theater" },
-        { primary: "Mend", secondary: "Eidola" },
-        { primary: "Magic 8-Ball", secondary: "Gazpacho" },
-        { primary: "Nightmares as Extensions of the Waking State", secondary: "Katatonia" },
-        { primary: "Power Train", secondary: "Majestica" },
-        { primary: "The Overview", secondary: "Steven Wilson" }
+        {
+          primary: "Borderland",
+          secondary: "Amorphis",
+          additionalText: "✍️✍️✍️, this latest one barely moved the needle for me. ✍️✍️✍️"
+        },
+        { primary: "Here There Be Dragons", secondary: "Avantasia", additionalText: "TODO" },
+        {
+          primary: "Futique",
+          secondary: "Biffy Clyro",
+          additionalText: "I am a huge fan of Biff's double-ish albums _A Celebration of Endings_ (2020) and _The Myth of the Happily Ever After_ (2021). Four years and the release of 2025's _Futique_ was a long time to wait for more Scottish alt-rock goodness. Sadly, I'll keep on waiting, hopefully not as long this time, for the next LP for these lads. _Futique_ did not make any sort of impression on me."
+        },
+        { primary: "Will You Haunt Me, With That Same Patience", secondary: "Bury Tomorrow", additionalText: "TODO" },
+        {
+          primary: "Parasomnia",
+          secondary: "Dream Theater",
+          additionalText: "After a string of Dream Theater albums that failed to capture my interest — the last one I enjoyed in full was _Black Clouds and Silver Linings_ from 2009! — I had high hopes that the return of once-estranged founder Mike Portnoy would be a return to progressive glory. After all, during Portnoy's sabbatical he played with a dozen other bands, seemingly heavily fertile ground for inspiration to bring back to his original true love.\n\nUnfortunately for me, Portnoy's journies abroad did not seem to coalesce with the other members' output into a return to their best work. I cannot remember a single thing about this album besides the chorus to \"Night Terror,\" which I can recall not because of good hooks or technical showmanship, but because singer James Labrie's nasally delivery of the track's title bordered on obnoxious.\n\nAnd the band is further dinged a couple extra points for using AI-generated album artwork. Shameful and embarrassing. Maybe this is just a band that no longer has good taste."
+        },
+        {
+          primary: "Mend",
+          secondary: "Eidola",
+          additionalText: "Last year's angry caustic twin to _Mend_, [_Eviscerate_](http://localhost:8080/lists/favoritealbums2024/#supp1-2), earned a place in my personal catalogue because of about half an album of material was so dominant and blistering (\"No Weapon Forged Shall Ever Prosper\" was good enough for [2024's sixth-best song](http://localhost:8080/lists/favoritesongs2024/#6)) made up for the half that was ✍️✍️✍️.\n\n_Mend_ is like _Eviscerate_ for me in that it has half an album of stuff that didn't grab me. Where _Mend_ differs is that its other half _also_ didn't grab me. That left me with... well, not much to enjoy.\n\nAll that said, I still think this band has top-tier chops and songwriting. I look forward to whatever they do next."
+        },
+        {
+          primary: "Magic 8-Ball",
+          secondary: "Gazpacho",
+          additionalText: "Man, this one stings. Gazpacho is a band that I would easily consider one of my all-time favorites. After almost three decades of albums that delighted with airy atmospheres, clever concepts, and dreamlike storytelling, I was fairly certain Gazpacho was firmly one of those bands who was incapable of releasing a bad album for me.\n\n_Magic 8-Ball_ proved me wrong. ✍️✍️✍️"
+        },
+        {
+          primary: "Nightmares as Extensions of the Waking State",
+          secondary: "Katatonia",
+          additionalText: "After 2023's _Sky Void of Stars_ didn't move the needle much for me, I was beginning to wonder if Katatonia's brand of progressive darkness was no longer for me. _City Burials_ before _Sky_ was good but , so that's where the slide started.     ️️✍️✍️✍️ "
+        },
+        {
+          primary: "Power Train",
+          secondary: "Majestica",
+          additionalText: "Power metal paragon Tommy Johansson is responsible for two of my favorite power metal LPs of the past several years, 2019's _Above the Sky_ and 2020's _A Christmas Carol_, the former I'd consider one of the great power metal albums of _all time._ So with 2024's news of his exit from Sabaton and his return to solo work in Majestica, I was pretty stoked to see what came next."
+        },
+        { primary: "The Overview", secondary: "Steven Wilson", additionalText: "TODO" }
       ]
     }
   ]
