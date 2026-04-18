006bb131cb2a46bdb9f12e1e0761f1d44b685dca
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2026-04-18 14:14:39 -0400

    Add Sleep Token songs data

---
+++ _data/lists/SleepTokenSongs.js
@@ -0,0 +1,243 @@
+export default {
+  title: "Sleep Token Songs Ranked",
+  "description": [],
+  type: {
+    primary: "song",
+    secondary: "album"
+  }, status: "released",
+  isCountdown: true,
+  zipStyle: {
+    colorStart: "gold",
+    colorEnd: "black"
+  }, items: [
+    {
+      primary: "Caramel",
+      secondary: "Even in Arcadia",
+      additionalText: ""
+    }, {
+      primary: "Euclid",
+      secondary: "Take Me Back to Eden",
+      additionalText: ""
+    },
+    {
+      primary: "The Summoning",
+      secondary: "Take Me Back to Eden",
+      additionalText: ""
+    },
+    {
+      primary: "Look To Windward",
+      secondary: "Even in Arcadia",
+      additionalText: ""
+    },
+    {
+      primary: "Chokehold",
+      secondary: "Take Me Back to Eden",
+      additionalText: ""
+    },
+    {
+      primary: "Mine",
+      secondary: "This Place Will Become Your Tomb",
+      additionalText: ""
+    },
+    {
+      primary: "The Night Does Not Belong To God",
+      secondary: "Sundowning",
+      additionalText: ""
+    },
+    {
+      primary: "Emergence",
+      secondary: "Even in Arcadia",
+      additionalText: ""
+    },
+    {
+      primary: "Take Me Back to Eden",
+      secondary: "Take Me Back to Eden",
+      additionalText: ""
+    },
+    {
+      primary: "Even in Arcadia",
+      secondary: "Even in Arcadia",
+      additionalText: ""
+    },
+    {
+      primary: "The Offering",
+      secondary: "Sundowning",
+      additionalText: ""
+    },
+    {
+      primary: "Higher",
+      secondary: "Sundowning",
+      additionalText: ""
+    },
+    {
+      primary: "Sugar",
+      secondary: "Sundowning",
+      additionalText: ""
+    },
+    {
+      primary: "Gethsemane",
+      secondary: "Even in Arcadia",
+      additionalText: ""
+    },
+    {
+      primary: "Infinite Baths",
+      secondary: "Even in Arcadia",
+      additionalText: ""
+    },
+    {
+      primary: "Damocles",
+      secondary: "Even in Arcadia",
+      additionalText: ""
+    },
+    {
+      primary: "Granite",
+      secondary: "Take Me Back to Eden",
+      additionalText: ""
+    },
+    {
+      primary: "Hypnosis",
+      secondary: "This Place Will Become Your Tomb",
+      additionalText: ""
+    },
+    {
+      primary: "Vore",
+      secondary: "Take Me Back to Eden",
+      additionalText: ""
+    },
+    {
+      primary: "Alkaline",
+      secondary: "This Place Will Become Your Tomb",
+      additionalText: ""
+    },
+    {
+      primary: "Dark Signs",
+      secondary: "Sundowning",
+      additionalText: ""
+    },
+    {
+      primary: "Atlantic",
+      secondary: "This Place Will Become Your Tomb",
+      additionalText: ""
+    },
+    {
+      primary: "Aqua Regia",
+      secondary: "Take Me Back to Eden",
+      additionalText: ""
+    },
+    {
+      primary: "Blood Sport",
+      secondary: "Sundowning",
+      additionalText: ""
+    },
+    {
+      primary: "Take Aim",
+      secondary: "Sundowning",
+      additionalText: ""
+    },
+    {
+      primary: "Missing Limbs",
+      secondary: "This Place Will Become Your Tomb",
+      additionalText: ""
+    },
+    {
+      primary: "Ascensionism",
+      secondary: "Take Me Back to Eden",
+      additionalText: ""
+    },
+    {
+      primary: "Past Self",
+      secondary: "Even in Arcadia",
+      additionalText: ""
+    },
+    {
+      primary: "Telomeres",
+      secondary: "This Place Will Become Your Tomb",
+      additionalText: ""
+    },
+    {
+      primary: "DYWTYLM",
+      secondary: "Take Me Back to Eden",
+      additionalText: ""
+    },
+    {
+      primary: "Dangerous",
+      secondary: "Even in Arcadia",
+      additionalText: ""
+    },
+    {
+      primary: "Fall For Me",
+      secondary: "This Place Will Become Your Tomb",
+      additionalText: ""
+    },
+    {
+      primary: "Rain",
+      secondary: "Take Me Back to Eden",
+      additionalText: ""
+    },
+    {
+      primary: "Provider",
+      secondary: "Even in Arcadia",
+      additionalText: ""
+    },
+    {
+      primary: "Are You Really Okay?",
+      secondary: "Take Me Back to Eden",
+      additionalText: ""
+    },
+    {
+      primary: "The Love You Want",
+      secondary: "This Place Will Become Your Tomb",
+      additionalText: ""
+    },
+    {
+      primary: "Distraction",
+      secondary: "This Place Will Become Your Tomb",
+      additionalText: ""
+    },
+    {
+      primary: "Say That You Will",
+      secondary: "Sundowning",
+      additionalText: ""
+    },
+    {
+      primary: "High Water",
+      secondary: "This Place Will Become Your Tomb",
+      additionalText: ""
+    },
+    {
+      primary: "The Apparition",
+      secondary: "Take Me Back to Eden",
+      additionalText: ""
+    },
+    {
+      primary: "Levitate",
+      secondary: "Sundowning",
+      additionalText: ""
+    },
+    {
+      primary: "Drag Me Under",
+      secondary: "Sundowning",
+      additionalText: ""
+    },
+    {
+      primary: "Like That",
+      secondary: "This Place Will Become Your Tomb",
+      additionalText: ""
+    },
+    {
+      primary: "Descending",
+      secondary: "This Place Will Become Your Tomb",
+      additionalText: ""
+    },
+    {
+      primary: "Give",
+      secondary: "Sundowning",
+      additionalText: ""
+    },
+    {
+      primary: "Gods",
+      secondary: "Sundowning",
+      additionalText: "The only Sleep Token song that I can't tolerate enough to listen through."
+    }
+  ]
+};
