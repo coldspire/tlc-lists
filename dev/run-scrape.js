f39b2a7d82f03df0aa4f0d6a7dd4c517835ea29b
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2026-04-18 13:53:45 -0400

    Add check for embed URL origin

--- dev/run-scrape.js
+++ dev/run-scrape.js
@@ -1,6 +1,11 @@
 import { argv } from "node:process";
 import getSongsFromEmbedUrl from "./fetch-apple-music-playlist.js";
 
+function isEmbedUrl(embedUrl) {
+  const url = new URL(embedUrl);
+  return url.origin.includes('embed');
+}
+
 async function getSongs(url) {
   return await getSongsFromEmbedUrl(url);
 }
@@ -9,7 +14,10 @@ async function getSongs(url) {
 const urlArgvIndex = 2;
 const embedUrlToScrape = argv[urlArgvIndex];
 if (!embedUrlToScrape) {
-  throw Error("Apple Music URL for embed playlist not included in script arguments");
+  throw Error("Apple Music URL for embedded playlist not found.");
+}
+if (!isEmbedUrl(embedUrlToScrape)) {
+  throw Error("The Apple Music URL doesn't appear to be for an embedded playlist. The origin must be \"embed.music.apple.com\".");
 }
 
 const songs = await getSongs(embedUrlToScrape);
