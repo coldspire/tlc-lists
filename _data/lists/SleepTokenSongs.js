dbc81085dd0cdb8cd08ca3d2adf435266bad0252
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2026-04-26 14:24:06 -0400

    Add some additional text

--- _data/lists/SleepTokenSongs.js
+++ _data/lists/SleepTokenSongs.js
@@ -1,10 +1,14 @@
 export default {
   title: "Sleep Token Songs Ranked",
-  "description": [],
+  "description": [
+    `The mysterious and tightly-wound band _Sleep Token_ has a body of work (four albums, as of 2026) that I enjoy muchly. The catalog is `,
+    `This list is available as a playlist (ordered from top-to-bottom) on [Apple Music 🎵](https://music.apple.com/us/playlist/sleep-token-ranked/pl.u-e98llj5Ib9DrV).`
+  ],
   type: {
     primary: "song",
     secondary: "album"
-  }, status: "released",
+  },
+  status: "released",
   isCountdown: true,
   zipStyle: {
     colorStart: "pink",
@@ -13,191 +17,191 @@ export default {
     {
       primary: "Caramel",
       secondary: "🦩 Even in Arcadia",
-      additionalText: ""
+      additionalText: "Has the light touch of any _Sleep Token_ ballad without any of the drippiness. And then when you're settled in, the song explodes into a dominating crunch that matches any of the heaviest songs in the catalog. Conceptually, the story of the peaks and valleys of ascendant showmanship is clear and empathetic. Just a tight package of everything that makes _Sleep Token_ great."
     }, {
       primary: "Euclid",
       secondary: "🗿 Take Me Back to Eden",
-      additionalText: ""
+      additionalText: "The gorgeous closer to _Eden_ is introspective and somber without being sappy. A buoyant piano backing lifts above its weight here."
     },
     {
       primary: "The Summoning",
       secondary: "🗿 Take Me Back to Eden",
-      additionalText: ""
+      additionalText: "The second single off TMBTE made us all go, _Oh, damn. This is an all-new level of Sleep Token._ A mammoth song that can't be played too loud, with its massive chugging rifts, soaring choruses, spacey guitar solos and ambient intermissions. The funky ending still kind of baffles me, but it also comes across as perfectly normal for a band that doesn't like sitting still."
     },
     {
       primary: "Look To Windward",
       secondary: "🦩 Even in Arcadia",
-      additionalText: ""
+      additionalText: "This song seems like the overture of a fantastic adventure, further illustrated by the Even in Arcadia artwork and social campaign that featured black flamingos and resplendent pink-ivy castles under siege."
     },
     {
       primary: "Chokehold",
       secondary: "🗿 Take Me Back to Eden",
-      additionalText: ""
+      additionalText: "If I think about this song, I can immediately hear that opening chord go _bbbwwwwwwaaaaaaaa_. Hypnotic."
     },
     {
       primary: "Mine",
       secondary: "🌊 This Place Will Become Your Tomb",
-      additionalText: ""
+      additionalText: "TPWBYT was my first Sleep Token album, and this song was my first favorite Sleep Token song."
     },
     {
       primary: "The Night Does Not Belong To God",
       secondary: "🌇 Sundowning",
-      additionalText: ""
+      additionalText: "There's something so sad and affecting about this song for me that I must be in the right mood to listen to it."
     },
     {
       primary: "Emergence",
       secondary: "🦩 Even in Arcadia",
-      additionalText: ""
+      additionalText: "This was the first single from EIA, and in a lot of ways it feels like the quintessential EIA song. And if the rhythmic riffs and rhyming wasn't good enough (it is), Gabi Rose comes in at the end with a splendid saxophone solo."
     },
     {
       primary: "Take Me Back to Eden",
       secondary: "🗿 Take Me Back to Eden",
-      additionalText: ""
+      additionalText: "One of the more progressive offerings, fitting a number of great segments into its 8-minute length. A journey."
     },
     {
       primary: "Even in Arcadia",
       secondary: "🦩 Even in Arcadia",
-      additionalText: ""
+      additionalText: "Love the gradual twinkling atmosphere and cautious piano that draws in the listener. And then comes in Vessel with possibly my favorite vocal performance of the catalog."
     },
     {
       primary: "The Offering",
       secondary: "🌇 Sundowning",
-      additionalText: ""
+      additionalText: "An early song that touched on the future power and flexibility of the band."
     },
     {
       primary: "Higher",
       secondary: "🌇 Sundowning",
-      additionalText: ""
+      additionalText: "A bit like a ballad that was twisted and compressed until it bled out its seams."
     },
     {
       primary: "Sugar",
       secondary: "🌇 Sundowning",
-      additionalText: ""
+      additionalText: "Big fan of the big chorus."
     },
     {
       primary: "Gethsemane",
       secondary: "🦩 Even in Arcadia",
-      additionalText: ""
+      additionalText: "A very progressive, inventive offering with some surprising vocal calisthenics."
     },
     {
       primary: "Infinite Baths",
       secondary: "🦩 Even in Arcadia",
-      additionalText: ""
+      additionalText: "A superb closer to EIA. Plays out like chapters in a book, and calls back to other songs on the album. A great trip."
     },
     {
       primary: "Damocles",
       secondary: "🦩 Even in Arcadia",
-      additionalText: ""
+      additionalText: "A pretty darn good straight-forward rock song."
     },
     {
       primary: "Granite",
       secondary: "🗿 Take Me Back to Eden",
-      additionalText: ""
+      additionalText: "The perfect slow groove after the big one-two clobbering of TMBTE's openers \"Chokehold\" and \"The Summoning.\""
     },
     {
       primary: "Hypnosis",
       secondary: "🌊 This Place Will Become Your Tomb",
-      additionalText: ""
+      additionalText: "A quintessential song with tight drumming and smooth vocals. A good first song for someone looking to get into the band."
     },
     {
       primary: "Vore",
       secondary: "🗿 Take Me Back to Eden",
-      additionalText: ""
+      additionalText: "Very enjoyably heavy."
     },
     {
       primary: "Alkaline",
       secondary: "🌊 This Place Will Become Your Tomb",
-      additionalText: ""
+      additionalText: "In a lot of ways, this is a big _Sleep Token_ song."
     },
     {
       primary: "Dark Signs",
       secondary: "🌇 Sundowning",
-      additionalText: ""
+      additionalText: "Stripped down and striking."
     },
     {
       primary: "Atlantic",
       secondary: "🌊 This Place Will Become Your Tomb",
-      additionalText: ""
+      additionalText: "The perfect opener to TPWBYT."
     },
     {
       primary: "Aqua Regia",
       secondary: "🗿 Take Me Back to Eden",
-      additionalText: ""
+      additionalText: "A great follow-up to the silky \"Granite\" on TMBTE, which combined brings us down gently from the double-track opening assault."
     },
     {
       primary: "Blood Sport",
       secondary: "🌇 Sundowning",
-      additionalText: ""
+      additionalText: "An aggressive pop-rock ripper."
     },
     {
       primary: "Take Aim",
       secondary: "🌇 Sundowning",
-      additionalText: ""
+      additionalText: "Feels a bit like a partially-successful experiment into alt-folk."
     },
     {
       primary: "Missing Limbs",
       secondary: "🌊 This Place Will Become Your Tomb",
-      additionalText: ""
+      additionalText: "In B-side territory here, but it's a good B-side. It just lacks a bit of the punch and percussive lyric delivery that I prefer in my _Sleep_ songs."
     },
     {
       primary: "Ascensionism",
       secondary: "🗿 Take Me Back to Eden",
-      additionalText: ""
+      additionalText: "Largely a (good) showcase of Vessel's rapping technique."
     },
     {
       primary: "Past Self",
       secondary: "🦩 Even in Arcadia",
-      additionalText: ""
+      additionalText: "I mean, it's a fine little electro-trap song. No missteps but no real draw either."
     },
     {
       primary: "Telomeres",
       secondary: "🌊 This Place Will Become Your Tomb",
-      additionalText: ""
+      additionalText: "Almost has a 70s prog feel to parts, especially in the guitar solo towards the end."
     },
     {
       primary: "DYWTYLM",
       secondary: "🗿 Take Me Back to Eden",
-      additionalText: ""
+      additionalText: "Pretty cute but could a bit more oomph. Or a shorter length."
     },
     {
       primary: "Dangerous",
       secondary: "🦩 Even in Arcadia",
-      additionalText: ""
+      additionalText: "Certainly sounds like a _Sleep Token_ song but lacks a hook for me."
     },
     {
       primary: "Fall For Me",
       secondary: "🌊 This Place Will Become Your Tomb",
-      additionalText: ""
+      additionalText: "Adorable and lightweight."
     },
     {
       primary: "Rain",
       secondary: "🗿 Take Me Back to Eden",
-      additionalText: ""
+      additionalText: "It's tough to argue against the high quality of TMBTE overall. That said, I can't remember this song as soon as I finish it."
     },
     {
       primary: "Provider",
       secondary: "🦩 Even in Arcadia",
-      additionalText: ""
+      additionalText: "I just don't think I like _Sleep Token_ ballads all that much, and this is one of the more ballad-y ones."
     },
     {
       primary: "Are You Really Okay?",
       secondary: "🗿 Take Me Back to Eden",
-      additionalText: ""
+      additionalText: "Reminds me of ballad made by a Guns N' Roses cover band. Not the worst thing in the world, but _Sleep Token_ is more interesting than that."
     },
     {
       primary: "The Love You Want",
       secondary: "🌊 This Place Will Become Your Tomb",
-      additionalText: ""
+      additionalText: "A romantic interlude that has very little to say."
     },
     {
       primary: "Distraction",
       secondary: "🌊 This Place Will Become Your Tomb",
-      additionalText: ""
+      additionalText: "I like how piano-driven it is."
     },
     {
       primary: "Say That You Will",
       secondary: "🌇 Sundowning",
-      additionalText: ""
+      additionalText: "A few of the _Sundowning_ songs feel like a band still figuring itself out. This is one of those songs."
     },
     {
       primary: "High Water",
@@ -207,37 +211,37 @@ export default {
     {
       primary: "The Apparition",
       secondary: "🗿 Take Me Back to Eden",
-      additionalText: ""
+      additionalText: "The lone song on TMBTE that fails to capture my attention. It's a bit airy."
     },
     {
       primary: "Levitate",
       secondary: "🌇 Sundowning",
-      additionalText: ""
+      additionalText: "Has all the germs of a good song but none of the grace."
     },
     {
       primary: "Drag Me Under",
       secondary: "🌇 Sundowning",
-      additionalText: ""
+      additionalText: "Has the tone and fading-out nature of an album closer, but is actually second-to-last (the much better \"Blood Sport\" is last)."
     },
     {
       primary: "Like That",
       secondary: "🌊 This Place Will Become Your Tomb",
-      additionalText: ""
+      additionalText: "Slithery and sullen in an obnoxious way."
     },
     {
       primary: "Descending",
       secondary: "🌊 This Place Will Become Your Tomb",
-      additionalText: ""
+      additionalText: "I can't get into this one beyond the beat."
     },
     {
       primary: "Give",
       secondary: "🌇 Sundowning",
-      additionalText: ""
+      additionalText: "Forgettable."
     },
     {
       primary: "Gods",
       secondary: "🌇 Sundowning",
-      additionalText: "The only Sleep Token song that I can't tolerate enough to listen through."
+      additionalText: "The only Sleep Token song that I can't tolerate enough to listen through. Almost salvaged by a pensive middle bit, but I can't handle the screaming anti-melodious harshness of the bookends."
     }
   ]
 };
